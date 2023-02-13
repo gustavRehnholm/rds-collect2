df806a2fded7ab423147db4b23b7ce980591e107
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-13 15:29:05 +0100

    Update script-inject.sh

--- parser/script-inject.sh
+++ parser/script-inject.sh
@@ -2,13 +2,23 @@
 
 # files with the stdout, 
 # mostly used for troubleshooting and getting meta information (How many files used for what)
-touch output/twitch/output-inject-rnd-1370.txt
-#touch output-info/twitch/output-inject-rnd-658.txt
-#touch output-info/twitch/output-inject-rnd-342.txt
-#touch output-info/twitch/output-inject-rnd-171.txt
+touch stdout-dir/output-inject-ittr-1370.txt
+#touch stdout-dir/output-inject-rnd-658.txt
+#touch stdout-dir/output-inject-rnd-342.txt
+#touch stdout-dir/output-inject-rnd-171.txt
+
+#touch stdout-dir/output-inject-rnd-1370.txt
+#touch stdout-dir/output-inject-rnd-658.txt
+#touch stdout-dir/output-inject-rnd-342.txt
+#touch stdout-dir/output-inject-rnd-171.txt
 
 # test injection with different captures sizes
-python3 inject-rnd.py 1370 | tee output/twitch/output-inject-rnd-1370.txt
-#python3 inject-rnd.py 658 | tee output-inject-rnd-658.txt
-#python3 inject-rnd.py 342 | tee output-inject-rnd-342.txt
-#python3 inject-rnd.py 171 | tee output-inject-rnd-171.txt
+python3 inject-ittr.py 1370 | tee stdout-dir/output-inject-ittr-1370.txt
+#python3 inject-ittr.py 658 | tee stdout-dir/output-inject-ittr-658.txt
+#python3 inject-ittr.py 342 | tee stdout-dir/output-inject-ittr-342.txt
+#python3 inject-ittr.py 171 | tee stdout-dir/output-inject-ittr-171.txt
+
+#python3 inject-rnd.py 1370 | tee stdout-dir/output-inject-rnd-1370.txt
+#python3 inject-rnd.py 658 | tee stdout-dir/output-inject-rnd-658.txt
+#python3 inject-rnd.py 342 | tee stdout-dir/output-inject-rnd-342.txt
+#python3 inject-rnd.py 171 | tee stdout-dir/output-inject-rnd-171.txt
