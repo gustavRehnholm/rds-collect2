72e2e4230d6a7430faad23f253eb91815548f7dc
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-14 20:47:07 +0100

    Update add_training_web_traffic.py

--- parser/add_training_web_traffic.py
+++ parser/add_training_web_traffic.py
@@ -3,7 +3,16 @@ import shutil
 
 def addTrain(webTrafficTrainFiles, parsedTrainFiles):
 
+    # add all web traffic train files
     for i in range(0, len(webTrafficTrainFiles)):
-        shutil.copy(webTrafficTrainFiles[0], parsedTrainFiles[0])
+        webTrafficFile = open(webTrafficTrainFiles[i], 'r') 
+        webTrafficLines = webTrafficFile.readlines()
+        webTrafficFile.close()
+
+        currParsedFile = open(parsedTrainFiles[i], 'a') 
+
+        # add every line from the web traffic file that is open
+        for j in range(0, len(webTrafficLines)):
+            currParsedFile.writelines(webTrafficLines[j])
 
     return True
