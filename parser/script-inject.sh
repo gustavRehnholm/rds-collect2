598879eab8c6bcab48e9455813e80a5d00d29766
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-14 13:11:47 +0100

    Update script-inject.sh

--- parser/script-inject.sh
+++ parser/script-inject.sh
@@ -2,39 +2,39 @@
 
 # files with the stdout, 
 # mostly used for troubleshooting and getting meta information (How many files used for what)
-#touch stdout-dir/output-inject-itr-1370.txt
-#touch stdout-dir/output-inject-ittr-685.txt
-#touch stdout-dir/output-inject-ittr-342.txt
-#touch stdout-dir/output-inject-ittr-171.txt
+touch stdout-dir/output-inject-itr-1370.txt
+touch stdout-dir/output-inject-ittr-685.txt
+touch stdout-dir/output-inject-ittr-342.txt
+touch stdout-dir/output-inject-ittr-220.txt
 
-#touch stdout-dir/output-inject-rnd-1370.txt
-#touch stdout-dir/output-inject-rnd-685.txt
-#touch stdout-dir/output-inject-rnd-342.txt
-#touch stdout-dir/output-inject-rnd-171.txt
+touch stdout-dir/output-inject-rnd-1370.txt
+touch stdout-dir/output-inject-rnd-685.txt
+touch stdout-dir/output-inject-rnd-342.txt
+touch stdout-dir/output-inject-rnd-220.txt
 
-rm stdout-dir/output-inject-none-1370.txt
-rm stdout-dir/output-inject-none-685.txt
-rm stdout-dir/output-inject-none-342.txt
-rm stdout-dir/output-inject-none-220.txt
+#rm stdout-dir/output-inject-none-1370.txt
+#rm stdout-dir/output-inject-none-685.txt
+#rm stdout-dir/output-inject-none-342.txt
+#rm stdout-dir/output-inject-none-220.txt
 
-touch stdout-dir/output-inject-none-1370.txt
-touch stdout-dir/output-inject-none-685.txt
-touch stdout-dir/output-inject-none-342.txt
-touch stdout-dir/output-inject-none-220.txt
+#touch stdout-dir/output-inject-none-1370.txt
+#touch stdout-dir/output-inject-none-685.txt
+#touch stdout-dir/output-inject-none-342.txt
+#touch stdout-dir/output-inject-none-220.txt
 
 
 # test injection with different captures sizes
-#script -q -c "python3 inject.py 1370 itr" /dev/null | tee stdout-dir/output-inject-itr-1370.txt
-#script -q -c "python3 inject-ittr.py 685" /dev/null | tee stdout-dir/output-inject-ittr-685.txt
-#script -q -c "python3 inject-ittr.py 342" /dev/null | tee stdout-dir/output-inject-ittr-342.txt
-#script -q -c "python3 inject-ittr.py 171" /dev/null | tee stdout-dir/output-inject-ittr-171.txt
+script -q -c "python3 inject.py 1370 itr" /dev/null | tee stdout-dir/output-inject-itr-1370.txt
+script -q -c "python3 inject-ittr.py 685" /dev/null | tee stdout-dir/output-inject-ittr-685.txt
+script -q -c "python3 inject-ittr.py 342" /dev/null | tee stdout-dir/output-inject-ittr-342.txt
+script -q -c "python3 inject-ittr.py 220" /dev/null | tee stdout-dir/output-inject-ittr-220.txt
 
-#script -q -c "python3 inject.py 1370 rnd" /dev/null | tee stdout-dir/output-inject-rnd-1370.txt
-#script -q -c "python3 inject-rnd.py 685" /dev/null | tee stdout-dir/output-inject-rnd-685.txt
-#script -q -c "python3 inject-rnd.py 342" /dev/null  | tee stdout-dir/output-inject-rnd-342.txt
-#script -q -c "python3 inject-rnd.py 171" /dev/null  | tee stdout-dir/output-inject-rnd-171.txt
+script -q -c "python3 inject.py 1370 rnd" /dev/null | tee stdout-dir/output-inject-rnd-1370.txt
+script -q -c "python3 inject-rnd.py 685" /dev/null | tee stdout-dir/output-inject-rnd-685.txt
+script -q -c "python3 inject-rnd.py 342" /dev/null  | tee stdout-dir/output-inject-rnd-342.txt
+script -q -c "python3 inject-rnd.py 220" /dev/null  | tee stdout-dir/output-inject-rnd-220.txt
 
-script -q -c "python3 inject.py 1370 none" /dev/null | tee stdout-dir/output-inject-none-1370.txt
-script -q -c "python3 inject.py 685 none" /dev/null | tee stdout-dir/output-inject-none-685.txt
-script -q -c "python3 inject.py 342 none" /dev/null | tee stdout-dir/output-inject-none-342.txt
-script -q -c "python3 inject.py 220 none" /dev/null | tee stdout-dir/output-inject-none-220.txt
+#script -q -c "python3 inject.py 1370 none" /dev/null | tee stdout-dir/output-inject-none-1370.txt
+#script -q -c "python3 inject.py 685 none" /dev/null | tee stdout-dir/output-inject-none-685.txt
+#script -q -c "python3 inject.py 342 none" /dev/null | tee stdout-dir/output-inject-none-342.txt
+#script -q -c "python3 inject.py 220 none" /dev/null | tee stdout-dir/output-inject-none-220.txt
