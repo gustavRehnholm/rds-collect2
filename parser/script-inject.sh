2f96580ad0c481fa82be1406bf511129260f4bb4
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-14 14:52:39 +0100

    Update script-inject.sh

--- parser/script-inject.sh
+++ parser/script-inject.sh
@@ -23,14 +23,14 @@ touch stdout-dir/output-inject-rnd-342.txt
 touch stdout-dir/output-inject-rnd-220.txt
 
 rm stdout-dir/output-inject-none-1370.txt
-#rm stdout-dir/output-inject-none-685.txt
-#rm stdout-dir/output-inject-none-342.txt
-#rm stdout-dir/output-inject-none-220.txt
+rm stdout-dir/output-inject-none-685.txt
+rm stdout-dir/output-inject-none-342.txt
+rm stdout-dir/output-inject-none-220.txt
 
 touch stdout-dir/output-inject-none-1370.txt
-#touch stdout-dir/output-inject-none-685.txt
-#touch stdout-dir/output-inject-none-342.txt
-#touch stdout-dir/output-inject-none-220.txt
+touch stdout-dir/output-inject-none-685.txt
+touch stdout-dir/output-inject-none-342.txt
+touch stdout-dir/output-inject-none-220.txt
 
 
 # test injection with different captures sizes
@@ -45,6 +45,6 @@ script -q -c "python3 inject.py 342 rnd"  /dev/null | tee stdout-dir/output-inje
 script -q -c "python3 inject.py 220 rnd"  /dev/null | tee stdout-dir/output-inject-rnd-220.txt
 
 script -q -c "python3 inject.py 1370 none" /dev/null | tee stdout-dir/output-inject-none-1370.txt
-#script -q -c "python3 inject.py 685 none" /dev/null | tee stdout-dir/output-inject-none-685.txt
-#script -q -c "python3 inject.py 342 none" /dev/null | tee stdout-dir/output-inject-none-342.txt
-#script -q -c "python3 inject.py 220 none" /dev/null | tee stdout-dir/output-inject-none-220.txt
+script -q -c "python3 inject.py 685 none" /dev/null | tee stdout-dir/output-inject-none-685.txt
+script -q -c "python3 inject.py 342 none" /dev/null | tee stdout-dir/output-inject-none-342.txt
+script -q -c "python3 inject.py 220 none" /dev/null | tee stdout-dir/output-inject-none-220.txt
