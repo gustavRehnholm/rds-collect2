9027602bb9b60730519c85cec1e3dc7593b7201b
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-19 23:30:47 +0100

    Update parse.py

--- parser/parse.py
+++ parser/parse.py
@@ -227,12 +227,12 @@ def main():
             if currLongestLossStreak >= 20:
                 print("This file is seen as broken and will not be part of the parsed dataset because: ")
                 print("The longest time of lost packets (", currLongestLossStreak, "), is over 20")
-                os.system("rm " + os.path.basename(currParsedFiles[0]))
+                os.system("rm rds-collect2/parser/parsed-noise/twitch/captures-1370/" + os.path.basename(currParsedFiles[0]))
                 rmFiles += 1
             elif currPercentLoss >= 0.05:
                 print("This file is seen as broken and will not be part of the parsed dataset because: ")
                 print("The percentage loss of packets (", currPercentLoss, "), is over 5 percent")
-                os.system("rm " + os.path.basename(currParsedFiles[0]))
+                os.system("rm rds-collect2/parser/parsed-noise/twitch/captures-1370/" + os.path.basename(currParsedFiles[0]))
                 rmFiles += 1
             else:
                 listLossPercent.append(currPercentLoss)
