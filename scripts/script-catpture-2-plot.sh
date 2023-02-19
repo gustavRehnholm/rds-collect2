084865a86fb7bc1566d206ac4b9d33a35b97bd27
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-19 22:20:30 +0100

    prepare for a longer test

---
+++ scripts/script-catpture-2-plot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Go from captures to plotted graphs
+# replicate the previous result
+
+# Parse, rm unusable data packets, and files with too much dataloss
+#python3 rds-collect2/parser/parse.py | tee rds-collect2/parser/stdout-dir/stdout-parse.txt
+
+# inject the data 
+./rds-collect2/scripts/script-inject.sh
+#script -q -c "python3 rds-collect2/parser/inject.py 1370 itr" /dev/null | tee rds-collect2/parser/stdout-dir/output-inject-itr-1370.txt
+
+# train DF, with the smaller datasample
+./rds-collect2/scripts/script-df.sh
+
+# plot the result
+./rds-collect2/scripts/script-plotter-old.sh
--- scripts/script-plotter-old.sh
+++ scripts/script-plotter-old.sh
@@ -2,15 +2,13 @@
 # Plot all the result graphs
 
 # input: {title} {csv file 1} {csv file 2} ...
+# wf-old-result/df-size-twitch-itr-1370.csv
 
 # DF with size
-python3 rds-collect2/parser/plot_df.py default-itr wf-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
-python3 rds-collect2/parser/plot_df.py default-rnd wf-result/df-size-twitch-rnd-1370.csv wf-result/df-size-twitch-rnd-685.csv wf-result/df-size-twitch-rnd-342.csv wf-result/df-size-twitch-rnd-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
 
 # DF without size
-python3 rds-collect2/parser/plot_df.py default-itr wf-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
-python3 rds-collect2/parser/plot_df.py default-rnd wf-result/df-size-twitch-rnd-1370.csv wf-result/df-size-twitch-rnd-685.csv wf-result/df-size-twitch-rnd-342.csv wf-result/df-size-twitch-rnd-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
 
 #DF tiktok
-python3 rds-collect2/parser/plot_df.py default-itr wf-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
-python3 rds-collect2/parser/plot_df.py default-rnd wf-result/df-size-twitch-rnd-1370.csv wf-result/df-size-twitch-rnd-685.csv wf-result/df-size-twitch-rnd-342.csv wf-result/df-size-twitch-rnd-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
