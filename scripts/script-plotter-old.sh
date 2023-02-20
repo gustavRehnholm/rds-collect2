9afa9f1b897c7a929261b6828caf72030bff6283
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-20 08:18:35 +0100

    Update script-plotter-old.sh

--- scripts/script-plotter-old.sh
+++ scripts/script-plotter-old.sh
@@ -5,10 +5,10 @@
 # wf-old-result/df-size-twitch-itr-1370.csv
 
 # DF with size
-python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/plotter/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
 
 # DF without size
-python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/plotter/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
 
 #DF tiktok
-python3 rds-collect2/parser/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
+python3 rds-collect2/plotter/plot_df.py default-itr wf-old-result/df-size-twitch-itr-1370.csv wf-result/df-size-twitch-itr-685.csv wf-result/df-size-twitch-itr-342.csv wf-result/df-size-twitch-itr-220.csv wf-result/df-size-twitch-none.csv
