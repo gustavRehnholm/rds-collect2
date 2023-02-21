0b9f24cd7f8f4d981f1c98491022beca591a407a
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-21 17:42:44 +0100

    test

--- scripts/script-df-test.sh
+++ scripts/script-df-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run all the wf that one needs, but with sample 5, to provide a quick test to see that the trainign works as intended
 
-# ./rds-collect2/parser/script-df-test.sh
+# ./rds-collect2/scripts/script-df-test.sh
 
 # ./df-fitness.py -d rds-collect2/parser/parsedFiles-old --csv df_old.csv
 
@@ -29,7 +29,7 @@
 ./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-rnd-342 --train --constant -s 5 --csv wf-test-result/df-constant-twitch-rnd-342.csv
 ./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-rnd-230 --train --constant -s 5 --csv wf-test-result/df-constant-twitch-rnd-220.csv
 
-./df-fitness.py -d rds-collect2/parser/injected-dataset/twitch/parsedFiles-none-1355 --train --constant -s 5 --csv wf-test-result/df-constant-twitch-none.csv
+./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-none-1355 --train --constant -s 5 --csv wf-test-result/df-constant-twitch-none.csv
 
 # DF tiktok
 ./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-itr-1355 --train --tiktok -s 5 --csv wf-test-result/df-tiktok-twitch-itr-1370.csv
@@ -42,4 +42,4 @@
 ./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-rnd-342 --train --tiktok -s 5 --csv wf-test-result/df-tiktok-twitch-rnd-342.csv
 ./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-rnd-230 --train --tiktok -s 5 --csv wf-test-result/df-tiktok-twitch-rnd-220.csv
 
-./df-fitness.py -d rds-collect2/parser/injected-dataset/twitch/parsedFiles-none-1355 --train --tiktok -s 5 --csv wf-test-result/df-tiktok-twitch-none.csv
+./df-fitness.py -d rds-collect2/parser/injected-datasets/twitch/parsedFiles-none-1355 --train --tiktok -s 5 --csv wf-test-result/df-tiktok-twitch-none.csv
