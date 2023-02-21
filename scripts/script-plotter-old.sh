8af8204c7e668df7173b5617296d92743dcfd65e
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-21 20:12:27 +0100

    added row to easy testing
    Testing of the different ways the injection can have on the result (which is unwanted)

--- scripts/script-plotter-old.sh
+++ scripts/script-plotter-old.sh
@@ -6,3 +6,5 @@
 
 # DF size, constant and tiktok
 python3 rds-collect2/plotter/plot_df.py old-result wf-old-result/df-size-twitch-itr-1355.csv wf-old-result/df-constant-twitch-itr-1355.csv wf-old-result/df-tiktok-twitch-itr-1355.csv
+
+#python3 rds-collect2/plotter/plot_df.py old-result wf-result/df-size-twitch-itr-1355.csv wf-result/df-constant-twitch-itr-1355.csv wf-result/df-tiktok-twitch-itr-1355.csv
