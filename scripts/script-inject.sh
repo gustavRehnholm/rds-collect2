72e9b67d90fceafa157c1f7fb26669de112a860c
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-22 08:50:53 +0100

    test

--- scripts/script-inject.sh
+++ scripts/script-inject.sh
@@ -28,12 +28,12 @@
 #script -q -c "python3 rds-collect2/parser/inject.py 342 itr" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-itr-342.txt &
 #script -q -c "python3 rds-collect2/parser/inject.py 230 itr" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-itr-230.txt &
 
-script -q -c "python3 rds-collect2/parser/inject.py 1355 rnd" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-1355.txt &
-script -q -c "python3 rds-collect2/parser/inject.py 685 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-685.txt &
-script -q -c "python3 rds-collect2/parser/inject.py 342 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-342.txt &
-script -q -c "python3 rds-collect2/parser/inject.py 230 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-230.txt &
+#script -q -c "python3 rds-collect2/parser/inject.py 1355 rnd" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-1355.txt &
+#script -q -c "python3 rds-collect2/parser/inject.py 685 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-685.txt &
+#script -q -c "python3 rds-collect2/parser/inject.py 342 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-342.txt &
+#script -q -c "python3 rds-collect2/parser/inject.py 230 rnd"  /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-rnd-230.txt &
 
-#script -q -c "python3 rds-collect2/parser/inject.py 1355 none" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-none-1355.txt &
+script -q -c "python3 rds-collect2/parser/inject.py 1355 none" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-none-1355.txt &
 #script -q -c "python3 rds-collect2/parser/inject.py 685 none" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-none-685.txt &
 #script -q -c "python3 rds-collect2/parser/inject.py 342 none" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-none-342.txt &
 #script -q -c "python3 rds-collect2/parser/inject.py 230 none" /dev/null | tee rds-collect2/parser/stdout-dir/inject/output-inject-none-230.txt &
